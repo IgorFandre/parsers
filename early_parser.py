a4df64fabe134d7553cff8e490d497d15b90c455
Igor Shalygin <igor@Igors-MacBook-Air.local>
2023-12-04 16:04:07 +0300

    Added regym for sentence parsing

--- early_parser.py
+++ early_parser.py
@@ -16,7 +16,7 @@ class Early_Situation:
                                 self.link == __value.link
 
     def __repr__(self) -> str:
-        return self.rule.left + " -> " + self.rule.right[:self.ptr_offset] + " * " + self.rule.left[self.ptr_offset:]
+        return f"{self.rule.left} -> {str(self.rule.right[:self.ptr_offset])} * {str(self.rule.right[self.ptr_offset:])}"
 
     def __str__(self) -> str:
         return self.__repr__()
@@ -27,7 +27,7 @@ class Early_Situation:
     def completed(self) -> bool:
         return self.ptr_offset == len(self.rule.right)
 
-    def get_cur_char(self) -> str:
+    def get_cur_token(self) -> str:
         return self.rule.right[self.ptr_offset]
     
     def move_offset(self):
@@ -46,15 +46,18 @@ class Earley_Parser:
 
         self.start = "&"
         self.grammar.non_terminals.add(self.start)
-        self.start_rule = Rule(self.start, self.grammar.start_symbol)
-        self.rules = {non_term : {Rule(rule.left.strip(), rule.right.strip())\
-                                    for rule in self.grammar.rules if rule.left.strip() == non_term}\
+        if self.grammar.regym == Grammatics.WORDS:
+            self.start_rule = Rule(self.start, self.grammar.start_token)
+        elif self.grammar.regym == Grammatics.SENTENCES:
+            self.start_rule = Rule(self.start, self.grammar.start_token.split())
+        self.rules = {non_term : {Rule(rule.left, rule.right)\
+                                    for rule in self.grammar.rules if rule.left == non_term}\
                                                     for non_term in self.grammar.non_terminals}
         self.rules.get(self.start, set()).add(self.start_rule)
         return self
 
-    def predict(self, word: str) -> bool:
-        self.situation_sets_list = [set() for _ in range(len(word) + 1)]
+    def predict(self, tokens) -> bool:
+        self.situation_sets_list = [set() for _ in range(len(tokens) + 1)]
         self.situation_sets_list[0].add(Early_Situation(self.start_rule))
 
         check = 0
@@ -65,8 +68,8 @@ class Earley_Parser:
             if check == len(self.situation_sets_list[0]): # Check if set changed
                 break
 
-        for index in range(len(word)): # First step. Try to read the symbol word[index] and fill situation_sets_list[index + 1]
-            self.__scan_situations(index + 1, word[index])
+        for index in range(len(tokens)): # First step. Try to read the symbol tokens[index] and fill situation_sets_list[index + 1]
+            self.__scan_situations(index + 1, tokens[index])
             while True:
                 check = len(self.situation_sets_list[index + 1])
                 self.__complete_situations(index + 1)
@@ -78,11 +81,11 @@ class Earley_Parser:
         answer = finish_situation in self.situation_sets_list[-1] # Check the answer
         return answer
 
-    def __scan_situations(self, index: int, symbol: str):
+    def __scan_situations(self, index: int, token: str):
         for situation in self.situation_sets_list[index - 1]:
             if situation.completed():
                 continue
-            if situation.get_cur_char() == symbol:
+            if situation.get_cur_token() == token:
                 self.situation_sets_list[index].add(situation.copy(mv_offset=1))
 
     def __predict_situations(self, index: int):
@@ -90,7 +93,7 @@ class Earley_Parser:
         for situation in self.situation_sets_list[index]:
             if situation.completed():
                 continue
-            cur_char = situation.get_cur_char()
+            cur_char = situation.get_cur_token()
             if not cur_char in self.grammar.non_terminals:
                 continue
             
@@ -107,7 +110,7 @@ class Earley_Parser:
             for return_trans in self.situation_sets_list[situation.link]:
                 if return_trans.completed():
                     continue
-                if return_trans.get_cur_char() == situation.rule.left:
+                if return_trans.get_cur_token() == situation.rule.left:
                     new_situation_set.add(return_trans.copy(mv_offset=1))
         self.situation_sets_list[index].update(new_situation_set)
 
@@ -115,19 +118,33 @@ if __name__ == '__main__':
     non_terminals = input().split()
     terminals = input().split()
 
+    regym = int(input()) # 0 for parse words and 1 for parse sentences
+
     rules_number = int(input())
     rules = []
-    for i in range(rules_number):
-        rules.append(Rule.parse_rule(input()))
+    
+    if regym == Grammatics.WORDS:
+        for i in range(rules_number):
+            rules.append(Rule.parse_rule_symbols(input()))
+    elif regym == Grammatics.SENTENCES:
+        for i in range(rules_number):
+            rules.append(Rule.parse_rule_words(input()))
+    else:
+        raise RuntimeError("Invalid regym")
 
-    start_symbol = input()
+    start_token = input()
+    if regym == Grammatics.WORDS and len(start_token) != 1:
+        raise RuntimeError(f"Invalid start_token '{start_token}'")
 
-    grammar = Grammatics(terminals, non_terminals, start_symbol, rules)
+    grammar = Grammatics(terminals, non_terminals, start_token, rules, regym)
 
     early_parser = Earley_Parser()
     early_parser.fit(grammar)
-    
-    words_number = int(input())
-    for i in range(words_number):
-        word = input()
-        print(early_parser.predict(word))
+
+    tokens_number = int(input())
+    for i in range(tokens_number):
+        if regym == Grammatics.WORDS:
+            tokens = input().strip()
+        elif regym == Grammatics.SENTENCES:
+            tokens = input().strip().split()
+        print(early_parser.predict(tokens))
--- grammatics.py
+++ grammatics.py
@@ -1,6 +1,6 @@
 class Rule:
     '''Rule - (left -> right) in Grammar'''
-    def __init__(self, left: str, right: str, id=0):
+    def __init__(self, left: str, right: list[str], id=0):
         self.id = id
         self.left = left
         self.right = right
@@ -9,7 +9,7 @@ class Rule:
         return self.left == __value.left and self.right == __value.right
 
     def __repr__(self) -> str:
-        return f"({self.id}: {self.left}->{self.right})"
+        return f"({str(self.id)}: {str(self.left)}->{str(self.right)})"
 
     def __str__(self) -> str:
         return self.__repr__()
@@ -18,15 +18,26 @@ class Rule:
         return hash(self.__repr__())
 
     @staticmethod
-    def parse_rule(string: str):
+    def parse_rule_symbols(string: str):
         rule = string.split("->")
         if len(rule) != 2:
             raise RuntimeError("Правила должны содержать только одну '->', разделяющую правую и левую части.")
         return Rule(rule[0].strip(), rule[1].strip())
 
+    @staticmethod
+    def parse_rule_words(string: str):
+        rule = string.split("->")
+        if len(rule) != 2:
+            raise RuntimeError("Правила должны содержать только одну '->', разделяющую правую и левую части.")
+        return Rule(rule[0].strip(), rule[1].strip().split())
+
 class Grammatics:
-    def __init__(self, terminals: list, non_terminals: list, start_symbol: str, rules: list):
-        self.start_symbol = start_symbol
+    WORDS = 0
+    SENTENCES = 1
+    def __init__(self, terminals: list[str], non_terminals: list[str], start_token: str, rules: list[Rule], regym: int):
+        self.start_token = start_token
+        
+        self.regym = regym
 
         self.terminals = set(terminals)
         self.non_terminals = set(non_terminals)
@@ -43,25 +54,23 @@ class Grammatics:
     def __str__(self) -> str:
         result = "Terminals: " + str(self.terminals) + "\n"
         result += "Nonterminals: " + str(self.non_terminals) + "\n"
-        result += "Start symbol: " + self.start_symbol + "\n"
+        result += "Start symbol: " + self.start_token + "\n"
         result += "Number of rules: %d\n" % len(self.rules)
         for rule in self.rules:
             result += str(rule) + "\n"
         return result
 
     def check_grammar(self):
-        if len(self.start_symbol) != 1:
-            raise RuntimeError("Стартовый символ {self.start_symbol} должен быть символом.")
-        if self.start_symbol not in self.non_terminals:
-            raise RuntimeError(f"Стартовый символ {self.start_symbol} должен быть нетерминалом {str(self.non_terminals)}.")
+        if self.start_token not in self.non_terminals:
+            raise RuntimeError(f"Стартовый токен {self.start_token} должен быть нетерминалом {str(self.non_terminals)}.")
         if len(set(self.terminals) & set(self.non_terminals)) != 0:
             raise RuntimeError(f"Терминалы и нетерминалы не должны пересекаться. В пересечении: {str(set(self.terminals) & set(self.non_terminals))}")
         for rule in self.rules:
-            if len(rule.left) != 1 or rule.left not in self.non_terminals:
-                raise RuntimeError(f"Левая часть правила {str(rule)} должна быть символом нетерминалом {self.non_terminals}.")
+            if rule.left not in self.non_terminals:
+                raise RuntimeError(f"Левая часть правила {str(rule)} должна быть нетерминалом {self.non_terminals}.")
             for symbol in rule.right:
                 if symbol not in self.non_terminals and symbol not in self.terminals:
-                    raise RuntimeError(f"Cимвол {symbol} в правиле {str(rule)} не является ни терминалом, ни нетерминалом.")
+                    raise RuntimeError(f"Токен {symbol} в правиле {str(rule)} не является ни терминалом, ни нетерминалом.")
 
     def get_rule(self, rule_id: int) -> Rule:
         for rule in self.rules:
